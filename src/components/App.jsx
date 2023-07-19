98f0748f918295ef7160fdfe7b2aa44e9d814f6b
Yaroslav Korin <lexuslx570vstoyota@gmail.com>
2023-07-19 08:42:22 +0300

    add_files_hw06

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,97 @@
+// import React, { Component } from 'react';
+
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
+
 export const App = () => {
+
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <section>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList/>
+    </section>
   );
 };
+
+
+
+// export class App extends Component {
+//   state = {
+//     contacts: [],
+//     filter: '',
+//   };
+
+//    componentDidMount() {
+//     const contacts = localStorage.getItem('contacts');
+//     const parsedContacts = JSON.parse(contacts);
+//     if (parsedContacts) {
+//       this.setState({ contacts: parsedContacts });
+//     }
+//   }
+
+//   componentDidUpdate(prevProps, prevState ) {
+//     if (this.state.contacts !== prevState.contacts) {
+//       console.log('renewed contacts');
+//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//     }
+//   }
+
+//   formSubmit = ({ name, number }) => {
+//     const contact = {
+//       id: nanoid(),
+//       name,
+//       number,
+//     };
+    
+//     const enterContact = this.state.contacts.some(
+//       i =>
+//         (i.name === contact.name.toLowerCase() &&
+//           i.number === contact.number) ||
+//         i.number === contact.number
+//     );
+    
+//     enterContact ? alert(`${name} is already in contacts`)
+//       : this.setState(({ contacts }) => ({
+//           contacts: [contact, ...contacts],
+//         }));
+//   };
+
+//   changeFilterInput = e => {
+//     this.setState({ filter: e.target.value });
+//   };
+
+//   findContacts = () => {
+//     const { filter, contacts } = this.state;
+//     return contacts.filter(contact =>
+//       contact.name.toLowerCase().includes(filter.toLowerCase())
+//     );
+//   };
+
+//   deleteContact = id => {
+//     this.setState(prevState => ({
+//       contacts: prevState.contacts.filter(contact => contact.id !== id),
+//     }));
+//   };
+
+//   render() {
+//     const { filter } = this.state;
+//     return (
+//       <section>
+//         <h1>Phonebook</h1>
+//         <ContactForm onSubmit={this.formSubmit} />
+//         <h2>Contacts</h2>
+//         <Filter filter={filter} changeFilterInput={this.changeFilterInput} />
+//         <ContactList
+//           contacts={this.findContacts()}
+//           deleteContact={this.deleteContact}
+//         />
+//       </section>
+//     );
+//   }
+// }
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,26 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { setFilter, getFilter } from '../../redux/slice';
+import './Filter.module.css';
+
+export const Filter = () => {
+const dispatch = useDispatch();
+  const filter = useSelector(getFilter);
+
+  const onChangeFilter = e => {
+    dispatch(setFilter(e.target.value));
+  };
+
+  return (
+    <div>
+      <label>
+        Find contacts by name
+        <input type="text" value={filter} onChange={onChangeFilter} />
+      </label>
+    </div>
+  );
+}
+
+// Filter.propTypes = {
+//   value: PropTypes.string,
+//   changeFilterInput: PropTypes.func,
+// };
