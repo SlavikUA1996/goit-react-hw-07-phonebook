99790ddbb2dcffdb7d377484fa6ec659977fc942
Yaroslav Korin <lexuslx570vstoyota@gmail.com>
2023-07-20 04:45:30 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -98,9 +98,7 @@ const [name, setName] = useState('');
         value={name}
         onChange={handleChange}
         placeholder="Name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        required
+       
       />
       <input
         type="tel"
@@ -108,9 +106,7 @@ const [name, setName] = useState('');
        value={phone}
         onChange={handleChange}
         placeholder="number"
-        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-        required
+        
       />
       <button type="submit">Add contact</button>
     </form>
